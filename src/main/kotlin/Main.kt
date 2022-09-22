7189e12bbaab31c70f609029fbd1d5654f73361e
tianye8 <tianye8@xiaomi.com>
2022-09-22 15:47:24 +0800

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -28,25 +28,28 @@ var student1: Student? = null
 //不指定初始值的情况，此时必须指定泛型
 var student: Student? by Weak()
 var student5 by Weak<Student>()
+
 //需要指定初始值的情况
 //自动推断出泛型
-var student2 by Weak{
-    Student("student2","2")
+var student2 by Weak {
+    Student("student2", "2")
 }
+
 //也可以指定泛型，一种是给属性指定类型，必须为可null的
 var student3: Student? by Weak {
-    Student("student3","3")
+    Student("student3", "3")
 }
+
 //第二种是为Weak指定泛型，不可null的
 var student4 by Weak<Student> {
-    Student("student4","4")
+    Student("student4", "4")
 }
 
 fun main(args: Array<String>) {
 
-    student = Student("dsjjkds","dskajfkls")
-    student5 = Student("student5","student5")
-    student1 = Student("student1","student1")
+    student = Student("dsjjkds", "dskajfkls")
+    student5 = Student("student5", "student5")
+    student1 = Student("student1", "student1")
     gc()
     println(student)
     println(student1)
@@ -79,4 +82,14 @@ fun main(args: Array<String>) {
 //    }?: "dsdksjf"
 //    println(test)
 //    println(if(test == null) "abc" else "dsdksjf")
+    var function = changeString("abc")
+    println(function("hello world"))
+    function = changeString("def")
+    println(function("hello world"))
+}
+
+fun changeString(prePrefix: String): (String) -> String {
+    return {
+        "$prePrefix & $it"
+    }
 }
